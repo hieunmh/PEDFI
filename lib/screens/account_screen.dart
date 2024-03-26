98f78320e05fbd31ac50bbd9b80d5968ddd2d379
hieunmh <hieunm.pt@gmail.com>
2024-03-26 11:31:52 +0700

    navbar icon

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pedfi/provider/dark_theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -8,17 +9,27 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
+
+    final Color color = themeState.getDarkTheme ? Colors.white70 : Colors.black87;
+
     return Scaffold(
       body: Center(
         child: SwitchListTile(
-          title: const Text('Theme'),
-          secondary: Icon(themeState.getDarkTheme
-              ? Icons.nightlight_round
-              : Icons.wb_sunny_outlined),
+          title: Text(
+            'Theme',
+            style: TextStyle(color: color),
+          ),
+          secondary: Icon(
+            themeState.getDarkTheme
+              ? CupertinoIcons.moon_stars_fill
+              : CupertinoIcons.sun_max_fill,
+            color: color,
+            ),
           onChanged: (bool value) {
             themeState.setDarkTheme = value;
           },
           value: themeState.getDarkTheme,
+          activeColor: Colors.white54,
         ),
       ),
     );
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,22 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:pedfi/bloc/screen_bloc.dart';
+import 'package:pedfi/provider/dark_theme_provider.dart';
 import 'package:pedfi/screens/account_screen.dart';
 import 'package:pedfi/screens/add_screen.dart';
 import 'package:pedfi/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 List<BottomNavigationBarItem> bottomNavItems = const <BottomNavigationBarItem>[
   BottomNavigationBarItem(
-    icon: Icon(Iconsax.home),
+    activeIcon: Icon(CupertinoIcons.house_alt_fill),
+    icon: Icon(CupertinoIcons.house_alt),
     label: 'Home',
   ),
   BottomNavigationBarItem(
-    icon: Icon(Iconsax.add),
+    activeIcon: Icon(CupertinoIcons.add_circled_solid),
+    icon: Icon(CupertinoIcons.add_circled),
     label: 'Add',
   ),
   BottomNavigationBarItem(
-    icon: Icon(Iconsax.user),
+    activeIcon: Icon(CupertinoIcons.person_fill),
+    icon: Icon(CupertinoIcons.person),
     label: 'Profile',
   ),
 ];
@@ -32,6 +37,10 @@ class Screens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    final Color color = themeState.getDarkTheme ? Colors.white70 : Colors.black87;
+
     return BlocConsumer<ScreenBloc, ScreenState>(
       listener: (context, state) {},
       builder: (context, state) {
@@ -41,7 +50,7 @@ class Screens extends StatelessWidget {
             items: bottomNavItems,
             currentIndex: state.screenIndex,
             selectedItemColor: const Color.fromRGBO(24, 119, 242, 1),
-            unselectedItemColor: Colors.grey,
+            unselectedItemColor: color,
             backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             onTap: (index) {
               BlocProvider.of<ScreenBloc>(context)
