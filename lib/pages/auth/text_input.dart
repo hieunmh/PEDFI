31f5c738971dfa61d2ea241f6243dc21be29732f
hieunmh <hieunm.pt@gmail.com>
2024-05-04 14:59:54 +0700

    feat: validate sign in

--- lib/pages/auth/text_input.dart
+++ lib/pages/auth/text_input.dart
@@ -5,20 +5,24 @@ import 'package:pedfi/pages/auth/auth_controller.dart';
 import 'package:pedfi/provider/dark_theme_provider.dart';
 import 'package:provider/provider.dart';
 
-class TextInput extends GetView<AuthController> {
+class InputPlaceholder extends GetView<AuthController> {
 
   final String hintText;
   final String placeholder;
   final bool obscureText;
   // ignore: prefer_typing_uninitialized_variables
   final ctrler;
+  final Color borderColor;
+  final String errorMsg;
 
-  const TextInput({
+  const InputPlaceholder({
     super.key,
     required this.hintText,
     required this.placeholder,
     required this.obscureText,
-    required this.ctrler
+    required this.ctrler,
+    required this.borderColor,
+    required this.errorMsg
   });
   
   @override
@@ -49,7 +53,8 @@ class TextInput extends GetView<AuthController> {
           decoration: BoxDecoration(
             color: themeState.getDarkTheme ? 
             const Color.fromRGBO(30, 30, 30, 1) : Colors.grey[200],
-            borderRadius: BorderRadius.circular(5)
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(color: borderColor, width: 1) 
           ),
           child: Padding(
             padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
@@ -70,8 +75,21 @@ class TextInput extends GetView<AuthController> {
               ),
             ),
           ),
-        )
+        ),
 
+        errorMsg.isNotEmpty ? Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              errorMsg,
+              style: const TextStyle(
+                color: AppColor.commonColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 12
+              ),
+            )
+          ],
+        ) : const SizedBox(height: 0),
       ],
     );
   }
