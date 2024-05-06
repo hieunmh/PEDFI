331ad01acb6666ccd15138eb6fa13d15715aba0c
hieunmh <hieunm.pt@gmail.com>
2024-05-07 02:30:54 +0700

    change: home ui

--- lib/pages/application/home/home_view.dart
+++ lib/pages/application/home/home_view.dart
@@ -18,73 +18,72 @@ class HomePage extends GetView<HomeController>  {
     final Color color = themeState.getDarkTheme ? 
     AppColor.textDarkThemeColor : AppColor.textLightThemeColor;
 
-    final Color bgcolor = themeState.getDarkTheme ? 
-    AppColor.bgDarkThemeColor : AppColor.bgLightThemeColor;
+    // final Color bgcolor = themeState.getDarkTheme ? 
+    // AppColor.bgDarkThemeColor : AppColor.bgLightThemeColor;
     
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-        backgroundColor: bgcolor,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    width: 60, 
-                    height: 60,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: const DecorationImage(
-                        image: AssetImage('assets/images/placeholder.jpg'),
-                        fit: BoxFit.cover,
-                      ),
-                      border: Border.all(color: Colors.grey, width: 0.2)
-                    ),
-                  ),
-        
-                  const SizedBox(width: 10),
-        
-                  Obx(() =>
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Welcome to pedfi!',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.grey
-                          ),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 60, 
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/placeholder.jpg'),
+                          fit: BoxFit.cover,
                         ),
-                        Text(
-                          controller.userEmail.value.isEmpty ? 
-                          'Sign in' : controller.userEmail.value,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: color
-                          ),
-                        )
-                      ],
+                        border: Border.all(color: Colors.grey, width: 0.2),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-        
-              const TotalBalance(),
-        
-              const SizedBox(height: 30),
-              
-              const Transaction()
-            ],
+          
+                    const SizedBox(width: 10),
+          
+                    Obx(() =>
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Welcome to pedfi!',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey
+                            ),
+                          ),
+                          Text(
+                            controller.userEmail.value.isEmpty ? 
+                            'Sign in' : controller.userEmail.value,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: color
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+          
+                const TotalBalance(),
+          
+                const SizedBox(height: 30),
+                
+                const Transaction()
+              ],
+            ),
           ),
-        ),
-      )
+        )
+      ),
     );
   }
 
--- lib/pages/application/home/total_balance.dart
+++ lib/pages/application/home/total_balance.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:pedfi/consts/app_color.dart';
 import 'package:pedfi/pages/application/home/home_controller.dart';
+import 'package:pedfi/provider/dark_theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class TotalBalance extends GetView<HomeController> {
   const TotalBalance({super.key});
@@ -10,115 +12,122 @@ class TotalBalance extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
 
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    
+    final Color color = themeState.getDarkTheme ? 
+    AppColor.textDarkThemeColor : AppColor.textLightThemeColor;
+
+    // final Color bgcolor = themeState.getDarkTheme ? 
+    // AppColor.bgDarkThemeColor : AppColor.bgLightThemeColor;
 
     return Container(
       width: double.infinity,
-      height: MediaQuery.of(context).size.width / 2,
       decoration: BoxDecoration(
-        color: AppColor.commonColor,
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(0),
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Total Balance',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w600
-            ),
-          ),
-
-          const Text(
-            '\$1100',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 40,
-              fontWeight: FontWeight.bold
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Center(
+            child: Column(
               children: [
-                Row(
-                  children: [
-                    Container(
-                      width: 30,
-                      height: 30,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Center(
-                        child: Icon(CupertinoIcons.arrow_down, size: 12),
-                      ),
-                    ),
-            
-                    const SizedBox(width: 10),
-            
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Incoming',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
-                        ),
-            
-                        Text(
-                          '\$2600',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                Text(
+                  '\$1234.56',
+                  style: TextStyle(
+                    color: color,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800
+                  )
                 ),
-
-                Row(
-                  children: [
-                    Container(
-                      width: 30,
-                      height: 30,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle
-                      ),
-                      child: const Center(
-                        child: Icon(CupertinoIcons.arrow_up, size: 12),
-                      ),
-                    ),
-            
-                    const SizedBox(width: 10),
-            
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Expenses',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
-                        ),
-            
-                        Text(
-                          '\$1500',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                Text(
+                  'Total balance',
+                  style: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontSize: 14,
+                  )
                 )
               ],
             ),
+          ),
+          
+          const SizedBox(height: 20),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: themeState.getDarkTheme ? 
+                    Colors.blue.shade800 : Colors.blue.shade400,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Incoming',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500
+                              ),
+                            ),
+                            Text(
+                              '+\$2600.10',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700
+                              ),
+                            )
+                          ],
+                        ),
+                      Icon(FontAwesomeIcons.arrowTrendUp, color: Colors.white, size: 20)
+                    ],
+                  ),
+                ),
+              ),
+          
+              const SizedBox(width: 10),
+          
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: themeState.getDarkTheme ? 
+                    Colors.red.shade800 : Colors.red.shade400,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Expense',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500
+                            ),
+                          ),
+                          Text(
+                            '-\$1000.25',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700
+                            ),
+                          )
+                        ],
+                      ),
+                      Icon(FontAwesomeIcons.arrowTrendDown, color: Colors.white, size: 20)
+                    ],
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
