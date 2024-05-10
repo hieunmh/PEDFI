fb21cba356ae6dfd3a402adde8c34c61306d9fa0
tronglinh23 <tronglinh230203@gmail.com>
2024-05-10 12:09:08 +0700

    stock ui - home screen - navbar

--- lib/pages/stock/stock_controller.dart
+++ lib/pages/stock/stock_controller.dart
@@ -1,6 +1,31 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class StockController extends GetxController {
-  
-  var count = 'Hello world'.obs;
+  final state = 0.obs;
+
+  late final PageController pageController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    pageController = PageController(initialPage: state.value);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  void handlePageChange(int index) {
+    log('Page changed to $index', name: 'Page Change');
+    state.value = index;
+  }
+
+  void handleNavBarTap(int index) {
+    log('Navigating to page $index', name: 'Navigation');
+    pageController.jumpToPage(index);
+  }
 }
--- lib/pages/stock/stock_view.dart
+++ lib/pages/stock/stock_view.dart
@@ -1,50 +1,137 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pedfi/consts/app_color.dart';
 import 'package:pedfi/pages/stock/stock_controller.dart';
 import 'package:pedfi/provider/dark_theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:pedfi/pages/stock/home.dart';
 
 class StockPage extends GetView<StockController> {
   const StockPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
     final themeState = Provider.of<DarkThemeProvider>(context);
 
-    final Color color = themeState.getDarkTheme ? 
-    AppColor.textDarkThemeColor : AppColor.textLightThemeColor;
-    
-    final Color bgcolor = themeState.getDarkTheme ? 
-    AppColor.bgDarkThemeColor : AppColor.bgLightThemeColor;
+    final Color textColor = themeState.getDarkTheme
+        ? AppColor.textDarkThemeColor
+        : AppColor.textLightThemeColor;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: bgcolor,
+    final Color bgColor = themeState.getDarkTheme
+        ? AppColor.bgDarkThemeColor
+        : AppColor.bgLightThemeColor;
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     backgroundColor: bgColor,
+    //   ),
+    //   body: Obx(
+    //     () => SingleChildScrollView(
+    //       child: Row(
+    //         children: [
+    //           Text(
+    //             'Stock',
+    //             style: TextStyle(color: color),
+    //           ),
+    //           Text(
+    //             controller.count.value,
+    //             style: TextStyle(color: color),
+    //           )
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: bgColor,
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller.pageController,
+        onPageChanged: (index) {
+          controller.handlePageChange(index);
+        },
+        children: const [Home(), Home(), Home(), Home()],
       ),
-      body: Obx(() =>
-        SingleChildScrollView(
-          child: Row(
-            children: [
-              Text(
-                'Stock screen',
-                style: TextStyle(
-                  color: color
-                ),
+      bottomNavigationBar: Obx(() => Container(
+            decoration: const BoxDecoration(
+                border:
+                    Border(top: BorderSide(color: Colors.grey, width: 0.5))),
+            child: Theme(
+              data: ThemeData(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
               ),
-
-              Text(
-                controller.count.value,
-                style:  TextStyle(
-                  color: color
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              child: BottomNavigationBar(
+                backgroundColor: bgColor,
+                currentIndex: controller.state.value,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                selectedItemColor: const Color(0xffFBC700),
+                unselectedItemColor: Colors.grey,
+                type: BottomNavigationBarType.fixed,
+                onTap: controller.handleNavBarTap,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icons/1.1.png',
+                      height: myHeight * 0.03,
+                      color: Colors.grey,
+                    ),
+                    label: '',
+                    activeIcon: Image.asset(
+                      'assets/icons/1.2.png',
+                      height: myHeight * 0.03,
+                      color: Color(0xffFBC700),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icons/2.1.png',
+                      height: myHeight * 0.03,
+                      color: Colors.grey,
+                    ),
+                    label: '',
+                    activeIcon: Image.asset(
+                      'assets/icons/2.2.png',
+                      height: myHeight * 0.03,
+                      color: Color(0xffFBC700),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icons/3.1.png',
+                      height: myHeight * 0.03,
+                      color: Colors.grey,
+                    ),
+                    label: '',
+                    activeIcon: Image.asset(
+                      'assets/icons/3.2.png',
+                      height: myHeight * 0.03,
+                      color: Color(0xffFBC700),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icons/4.1.png',
+                      height: myHeight * 0.03,
+                      color: Colors.grey,
+                    ),
+                    label: '',
+                    activeIcon: Image.asset(
+                      'assets/icons/4.2.png',
+                      height: myHeight * 0.03,
+                      color: Color(0xffFBC700),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
+    ));
   }
-
 }
